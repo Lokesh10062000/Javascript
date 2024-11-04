57eb1ed1dffce05ed206abf6da6b3b9e92a3553b
Lokesh10062000 <chlokeshvarmam003@gmail.com>
2024-11-04 17:59:41 +0530

    introduction

---
+++ FirstProgram.js
@@ -0,0 +1,6 @@
+// console :-
+//         By writing console .log we can see the results in the inpect tab of the browser console.
+console.log("welcome to javascript");
+
+//By writing document .write we can print the data in the tab
+document.write("hello lokesh");
